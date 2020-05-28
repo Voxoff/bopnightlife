824549258848f4cb72de7291d8970d952e1dbb17
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2020-05-28 12:54:23 +0100

    refactor

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
-  def home
-  end
-
   def subscribe
     unless email =~ Devise.email_regexp
       return flash[:notice] = "The email seems to be invalid"
