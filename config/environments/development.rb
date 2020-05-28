fa7d34637f83f49bd4c371206c7cbfd021e306a1
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2020-05-28 22:48:24 +0100

    styling of cards

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,10 +6,7 @@ Rails.application.configure do
 
   config.after_initialize do
     Bullet.enable = true
-    # Bullet.sentry = true
-    # Bullet.bullet_logger = true
     Bullet.rails_logger = true
-    # Bullet.add_footer = true
     Bullet.skip_html_injection = false
   end
 
