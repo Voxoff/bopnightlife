2300f79ca2fe8e37245d4790dbe80d2321675212
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2020-05-28 11:57:00 +0100

    code generation

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,4 +1,11 @@
 class Code < ApplicationRecord
   belongs_to :promotion
-  scope :stale, () -> { where(activated: false).where("updated_at < ?", 7.days.ago) }
+  scope :stale, -> { where(activated: false).where("updated_at < ?", 7.days.ago) }
+
+  after_create :generate_code
+
+  def generate_code
+    code = id.to_s + ('A'..'Z').to_a.shuffle[0,3].join
+    update(code: code)
+  end
 end
