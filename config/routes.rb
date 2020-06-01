d475c8c2a9327c1ef76c2ef4b5de51dcfd65911c
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2020-06-01 15:47:38 +0100

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, ActiveAdmin::Devise.config
   root to: 'nightclubs#index'
 
   resources :promotions, only: :show
