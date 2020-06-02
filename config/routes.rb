bc76606b91a5274b18bb63f52844780feecd07e8
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2020-06-02 14:06:08 +0100

    this adds two separate logins so remove

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
-  devise_for :users, ActiveAdmin::Devise.config
+  devise_for :users
   root to: 'nightclubs#index'
 
   resources :promotions, only: :show
