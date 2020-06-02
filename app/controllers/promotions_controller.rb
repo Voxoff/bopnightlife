4a33262f393fb33231bf8f817dbeb8d8fd7722a9
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2020-06-02 10:57:54 +0100

    address up and style

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -5,12 +5,11 @@ class PromotionsController < ApplicationController
     @promotion = Promotion.find(params[:id])
     @code = Code.create(promotion: @promotion)
 
-    require 'rqrcode'
     qrcode = RQRCode::QRCode.new(@code.code)
     @svg = qrcode.as_svg(
       offset: 0,
       color: '000',
       shape_rendering: 'crispEdges',
-      )
+    )
   end
 end
